dacdbd153f984725e38643fdafdb7e604507fb5c
jorgevs.mx <jorgevs.mx@gmail.com>
2020-10-24 21:27:36 -0400

    adding some changes

--- examples/raylib/main.cpp
+++ examples/raylib/main.cpp
@@ -30,6 +30,21 @@ int main() {
 	raylib::Color textColor(LIGHTGRAY);
 	raylib::Window w(screenWidth, screenHeight, "raylib [core] example - basic window");
 
+	// Box A: Moving box
+	raylib::Rectangle boxA(10, GetScreenHeight() / 2 - 50, 200, 100);
+	int boxASpeedX = 4;
+
+	// Box B: Mouse moved box
+	raylib::Rectangle boxB(GetScreenWidth() / 2 - 30, GetScreenHeight() / 2 - 30, 60, 60);
+
+	raylib::Rectangle boxCollision(0); // Collision rectangle
+	///raylib::Rectangle boxCollision(0,0); // Collision rectangle
+
+	int screenUpperLimit = 40;      // Top menu limits
+
+	bool pause = false;             // Movement pause
+	bool collision = false;         // Collision detection
+
 	SetTargetFPS(60);
 	//--------------------------------------------------------------------------------------
 
@@ -38,16 +53,60 @@ int main() {
 	{
 		// Update
 		//----------------------------------------------------------------------------------
-		// TODO: Update your variables here
+		
+		// Move box if not paused
+		if (!pause) boxA.x += boxASpeedX;
+
+		// Bounce box on x screen limits
+		if (((boxA.x + boxA.width) >= GetScreenWidth()) || (boxA.x <= 0)) boxASpeedX *= -1;
+
+		// Update player-controlled-box (box02)
+		boxB.x = GetMouseX() - boxB.width / 2;
+		boxB.y = GetMouseY() - boxB.height / 2;
+
+		// Make sure Box B does not go out of move area limits
+		if ((boxB.x + boxB.width) >= GetScreenWidth()) boxB.x = GetScreenWidth() - boxB.width;
+		else if (boxB.x <= 0) boxB.x = 0;
+
+		if ((boxB.y + boxB.height) >= GetScreenHeight()) boxB.y = GetScreenHeight() - boxB.height;
+		else if (boxB.y <= screenUpperLimit) boxB.y = screenUpperLimit;
+
+		// Check boxes collision
+		collision = boxA.CheckCollision(boxB);
+
+		// Get collision rectangle (only on collision)
+		if (collision) boxCollision = boxA.GetCollision(boxB);
+
+		// Pause Box A movement
+		if (IsKeyPressed(KEY_SPACE)) pause = !pause;
 		//----------------------------------------------------------------------------------
 
 		// Draw
 		//----------------------------------------------------------------------------------
 		BeginDrawing();
 
-		background.ClearBackground();
+		//background.ClearBackground();
+		textColor.DrawText("Congrats! You created your first window!", 190, 80, 20);
 
-		textColor.DrawText("Congrats! You created your first window!", 190, 200, 20);
+		ClearBackground(RAYWHITE);
+
+		DrawRectangle(0, 0, screenWidth, screenUpperLimit, collision ? RED : BLACK);
+
+		boxA.Draw(GOLD);
+		boxB.Draw(BLUE);
+
+		if (collision) {
+			// Draw collision area
+			boxCollision.Draw(LIME);
+
+			// Draw collision message
+			DrawText("COLLISION!", GetScreenWidth() / 2 - MeasureText("COLLISION!", 20) / 2, screenUpperLimit / 2 - 10, 20, BLACK);
+
+			// Draw collision area
+			DrawText(FormatText("Collision Area: %i", (int)boxCollision.width * (int)boxCollision.height), GetScreenWidth() / 2 - 100, screenUpperLimit + 10, 20, BLACK);
+		}
+
+		DrawFPS(10, 10);
 
 		EndDrawing();
 		//----------------------------------------------------------------------------------
